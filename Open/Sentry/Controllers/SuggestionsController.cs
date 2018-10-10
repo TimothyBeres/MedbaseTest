9c3d0908d9443d722c87ef8f17935668903c8bb3
Timothy Beres <tibere@ttu.ee>
2018-10-10 14:35:14 +0300

    New view(suggestions)

---
+++ Open/Sentry/Controllers/SuggestionsController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Open.Aids;
+using Open.Core;
+using Open.Data.Location;
+using Open.Domain.Location;
+using Open.Facade.Location;
+using Sentry1.Models;
+
+namespace Open.Sentry1.Controllers
+{
+    public class SuggestionsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+
+    }
+}
